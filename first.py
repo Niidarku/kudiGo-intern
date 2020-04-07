6be5797b2c9453798bc9a38d3fe2fd7e25d5d542
niidarku <niidarku66@gmail.com>
2020-04-07 15:14:20 +0000

    made change to link in line 9

---
+++ first.py
@@ -0,0 +1,10 @@
+class Web
+
+
+def __init__(self, name, website, location):
+    self.name= name
+    self.website= website
+    self.location= location
+
+my_web= Web("Niidarku", "https://kudigo.com", "Accra Digital Centre")
+print(my_web.name)
